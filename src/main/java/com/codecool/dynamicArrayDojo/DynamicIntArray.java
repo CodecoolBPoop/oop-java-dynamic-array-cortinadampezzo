6b772d4054fc3dd920323c5aa221c527edb9347d
cortinadampezzo <kisgans@gmail.com>
2019-08-20 15:01:57 +0200

    refactor

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,9 +1,39 @@
 package com.codecool.dynamicArrayDojo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // put your code here!
 public class DynamicIntArray {
+    private Integer[] dynamicArray;
+    private Integer realSize = 0;
+
+    public DynamicIntArray(int startSize) {
+        dynamicArray = new Integer[startSize];
+
+    }
+
+    public DynamicIntArray() {
+        this(10);
+    }
+
     @Override
     public String toString() {
-        return "Implement me plz.. Pretty plz..";
+        StringBuilder sb = new StringBuilder();
+        for (Integer item : dynamicArray) {
+            sb.append(" ").append(item);
+        }
+        return sb.toString();
+    }
+
+    public void add(int newElement) {
+        // size
+        //add
+    }
+
+    public void remove(int i) {
+    }
+
+    public void insert(int i, int i1) {
     }
 }
